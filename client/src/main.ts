d41dcf5d5c65e0acc155040bbc70cd73994e4c88
Nick Boschi <boschin1@newpaltz.edu>
2022-10-06 15:11:25 -0400

    in class 10/6

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,6 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// import ttt, {add, subtract} from './stores/temp';
+// console.log(ttt);
+
+// const bigger = add(ttt, 1);
+// console.log(bigger);
+
+// let reducer = 0;
+// if(reducer != null) {
+//     const smaller = subtract(ttt, 2);
+//     console.log(smaller);
+// }
 
 import './assets/main.scss'
 
--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -1,15 +1,40 @@
+<script setup lang="ts">
+  const items = [
+    {name: 'Shoes', cost: 30},
+    {name: 'Belt', cost: 20},
+    {name: 'Socks', cost: 10}
+  ]
+
+  const cart = [
+    {name: 'Shoes', cost: 30},
+    {name: 'Belt', cost: 20}
+  ]
+
+</script>
+
+
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="title">This is an about page</h1>
+
+    <ul>
+      <li v-for="item in items" :key="item.name">
+        <h3>{{item.name}}</h3> 
+        <em>${{item.cost}}</em>
+      </li>
+
+    </ul>
+
+
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
+<style lang="scss">
+  ul {
     display: flex;
-    align-items: center;
+    width: 200px;
+    padding: 5px;
+    margin: 5px;
   }
-}
+
 </style>
